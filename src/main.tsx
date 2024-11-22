188cdfdcec6d8af5b00a6a65fe88775559bb82ab
Evan Harris <echarris@smcm.edu>
2024-11-22 15:12:35 -0500

    Initial commit

---
+++ src/main.tsx
@@ -0,0 +1,67 @@
+import { Plugin, WorkspaceLeaf, ItemView } from 'obsidian';
+import { createRoot, Root } from 'react-dom/client';
+import { StrictMode } from 'react';
+import { ReactView } from './views/ReactView';
+
+const VIEW_TYPE_EXAMPLE = 'example-react-view';
+
+class ExampleReactView extends ItemView {
+    root: Root | null = null;
+
+    constructor(leaf: WorkspaceLeaf) {
+        super(leaf);
+    }
+
+    getViewType() {
+        return VIEW_TYPE_EXAMPLE;
+    }
+
+    getDisplayText() {
+        return 'Example React View';
+    }
+
+    async onOpen() {
+        const { containerEl } = this;
+        this.root = createRoot(containerEl);
+        this.root.render(
+            <StrictMode>
+                <ReactView />
+            </StrictMode>
+        );
+    }
+
+    async onClose() {
+        this.root?.unmount();
+    }
+}
+
+export default class MyReactPlugin extends Plugin {
+    async onload() {
+        this.registerView(
+            VIEW_TYPE_EXAMPLE,
+            (leaf) => new ExampleReactView(leaf)
+        );
+
+        this.addRibbonIcon('dice', 'Open Example React View', () => {
+            this.activateView();
+        });
+    }
+
+    async activateView() {
+        const { workspace } = this.app;
+        
+        let leaf = workspace.getLeavesOfType(VIEW_TYPE_EXAMPLE)[0];
+        if (!leaf) {
+            leaf = workspace.getLeaf(false);
+            await leaf.setViewState({
+                type: VIEW_TYPE_EXAMPLE,
+                active: true,
+            });
+        }
+        workspace.revealLeaf(leaf);
+    }
+
+    onunload() {
+        this.app.workspace.detachLeavesOfType(VIEW_TYPE_EXAMPLE);
+    }
+}
---
+++ src/views/ReactView.tsx
@@ -0,0 +1,8 @@
+export const ReactView: React.FC = () => {
+    return (
+        <div>
+            <h1>Hello from React in Obsidian!</h1>
+            <p>This is a React component rendered within an Obsidian plugin.</p>
+        </div>
+    );
+};
